a0152a75ddcc14fd000f054733e2834b87aead81
Eddcakes <eddadamson@gmail.com>
2021-09-06 16:50:55 +0100

    WIP adding rooms

--- client/src/components/Client.js
+++ client/src/components/Client.js
@@ -1,38 +1,54 @@
 import './Client.css';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { w3cwebsocket as W3CWebSocket } from 'websocket';
 import { useUser } from '../hooks/useUser';
 
 const wsPort = process.env.REACT_APP_SERVER_PORT || 8000;
-const client = new W3CWebSocket(`ws://localhost:${wsPort}/ws`);
+// const client = new W3CWebSocket(`ws://localhost:${wsPort}/ws`);
 
 export function Client() {
   const { user } = useUser();
+  const websocket = useRef(null);
+  const [memberList, setMemberList] = useState([]);
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
   const handleInputChange = (evt) => {
     setMessage(evt.target.value);
   };
-
   const handleClick = (evt) => {
     evt.preventDefault();
-    client.send(JSON.stringify({ user, message }));
+    websocket.current.send(
+      JSON.stringify({ user, message, action: 'message' })
+    );
   };
 
   useEffect(() => {
-    client.onopen = () => {
+    websocket.current = new W3CWebSocket(`ws://localhost:${wsPort}/ws`);
+    websocket.current.onopen = () => {
       console.log('WebSocket Client Connected');
+      // send join event to add to room on server
+      websocket.current.send(JSON.stringify({ user, action: 'join' }));
     };
-    client.onmessage = ({ data }) => {
+    websocket.current.onmessage = ({ data }) => {
       const msg = JSON.parse(data);
-      setMessages((prev) => {
-        return [...prev, msg];
-      });
+      switch (msg.action) {
+        case 'users':
+          console.log(msg);
+          setMemberList(msg.data);
+          break;
+        case 'message':
+          setMessages((prev) => {
+            return [...prev, msg.message];
+          });
+          break;
+        default:
+          console.error('Cannot find data.action for socket message');
+      }
     };
     return () => {
-      client.close();
+      websocket.current.close();
     };
-  }, []);
+  }, [user]);
   return (
     <div className='App'>
       <header className='App-header'>
@@ -40,6 +56,16 @@ export function Client() {
       </header>
       <div>Nick: {user.nick}</div>
       <div>Room: {user.room}</div>
+      <div>User list:</div>
+      <ul>
+        {memberList.map((member) => {
+          return (
+            <li key={member.userId}>
+              {member.nick} - {member.userId}
+            </li>
+          );
+        })}
+      </ul>
       <form>
         <label htmlFor='message'>message</label>
         <input name='message' value={message} onChange={handleInputChange} />
--- server/ws/game.ts
+++ server/ws/game.ts
@@ -1,33 +1,96 @@
 import {
-  WebSocket,
   isWebSocketCloseEvent,
+  WebSocket,
 } from 'https://deno.land/std/ws/mod.ts';
 
+interface IBroadcastEvent {
+  nick: string;
+}
+
+interface IUser {
+  userId: string;
+  nick: string;
+  room: string;
+  ws: WebSocket;
+}
+
+interface IMsgEvent {
+  user: IUser;
+  action: string;
+  message: string;
+}
+
 const sockets = new Map<string, WebSocket>();
+
+const users = new Map<string, IUser>();
+
+const rooms = new Map();
+
 const broadcastEvent = (message: IBroadcastEvent) => {
   sockets.forEach((ws: WebSocket) => {
     ws.send(JSON.stringify(message));
   });
 };
 
+const roomBroadcastEvent = (roomName: string) => {
+  console.log('broadcast to: ', roomName);
+  const users = rooms.get(roomName) || [];
+  for (const user of users) {
+    const event = {
+      action: 'users',
+      data: getDisplayUsers(roomName),
+    };
+    console.log('send users event', event);
+    user.ws.send(JSON.stringify(event));
+  }
+};
+
+function getDisplayUsers(roomName: string) {
+  const users = rooms.get(roomName) || [];
+  return users.map((user: IUser) => ({ userId: user.userId, nick: user.nick }));
+}
+
 export const gameConnection = async (ws: WebSocket) => {
   console.log('new socket connection');
   const uid = globalThis.crypto.randomUUID();
+  // to be nuked in favour for users
   sockets.set(uid, ws);
 
   for await (const evt of ws) {
     if (isWebSocketCloseEvent(evt)) {
       // what if its accidental close? how can we let user reconnect
+      console.log('closed socket connection');
       sockets.delete(uid);
+      const thisUser = users.get(uid);
+      const roomUsers = rooms.get(thisUser?.room) || [];
+      const existing = roomUsers.filter((u: any) => u.userID !== uid);
+      rooms.set(thisUser?.room, existing);
     }
     // msg from client
     if (typeof evt === 'string') {
       const parsedEvt = JSON.parse(evt);
-      broadcastEvent(parsedEvt);
+      switch (parsedEvt.action) {
+        case 'join':
+          const newUser = {
+            userId: uid,
+            nick: parsedEvt.user.nick,
+            room: parsedEvt.user.room,
+            ws,
+          };
+          users.set(uid, newUser);
+          const usersInRoom = rooms.get(parsedEvt.user.room) || [];
+          usersInRoom.push(newUser);
+          rooms.set(parsedEvt.user.room, usersInRoom);
+          roomBroadcastEvent(parsedEvt.user.room);
+          break;
+        case 'message':
+          console.log(parsedEvt);
+          /* roomBroadcastEvent works on user join, now to make it work for messages */
+          broadcastEvent(parsedEvt);
+          break;
+        default:
+          console.error('Cannot find action for socket event');
+      }
     }
   }
 };
-
-interface IBroadcastEvent {
-  nick: string;
-}
